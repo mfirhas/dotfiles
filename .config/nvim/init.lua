bff0cb21af9acc8dc48328c61f38a1f0aa003281
mfirhas <mfathirirhas@gmail.com>
2024-06-22 10:46:32 +0700

    Edit nvim configs

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -22,4 +22,16 @@ vim.g.netrw_liststyle = 3
 -- Enable search highlighting
 vim.opt.hlsearch = true
 
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+vim.g.mapleader = "'"
+
+-- Set up Telescope file browser
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', ':Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fb', ':Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', ':Telescope help_tags<CR>', { noremap = true, silent = true })
+
 require("config.lazy")
