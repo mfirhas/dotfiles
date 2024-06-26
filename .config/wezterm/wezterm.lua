6ba61b7c2a39bb0feb84a719450a623e35ffda30
mfirhas <mfathirirhas@gmail.com>
2024-06-27 00:26:47 +0700

    Add panes toggle for wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,7 +6,6 @@ local background_color = '1d2129'
 local foreground_color = 'ffffff'
 local selection_bg = 'c1ddff'
 local selection_fg = 'black'
-local act = wezterm.action
 
 local config = wezterm.config_builder()
 config.color_scheme = color_scheme
@@ -37,33 +36,139 @@ config.window_padding = {
   bottom = 0,
 }
 
+-- split panes in multiple directions, and toggle them, used for toggle 1 extra pane
+function toggle_split(direction)
+  return function(_, pane)
+      local tab = pane:tab()
+      local panes = tab:panes_with_info()
+      if #panes == 1 then
+        pane:split({
+            direction = direction,
+            size = 0.5,
+        })
+      elseif direction == 'Top' or direction == 'Left' then
+        if not panes[2].is_zoomed then
+          panes[2].pane:activate()
+          tab:set_zoomed(true)
+        elseif panes[2].is_zoomed then
+          tab:set_zoomed(false)
+          panes[1].pane:activate()
+        end
+      else
+        if not panes[1].is_zoomed then
+          panes[1].pane:activate()
+          tab:set_zoomed(true)
+        elseif panes[1].is_zoomed then
+          tab:set_zoomed(false)
+          panes[2].pane:activate()
+        end
+      end
+  end
+end
+
 config.keys = {
+  -- Open new pane at bottom, starting new session
+  {
+    key = '"',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  -- Open new pane at right, starting new session
+  {
+    key = '}',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  -- Open new pane at top, starting new session
+  {
+    key = '|',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action_callback(function(_, pane)
+      pane:split({
+          direction = "Top",
+          size = 0.5,
+          domain = 'CurrentPaneDomain',
+      })
+    end),
+  },
+  -- Open new pane at left, starting new session
+  {
+    key = '{',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action_callback(function(_, pane)
+      pane:split({
+          direction = "Left",
+          size = 0.5,
+          domain = 'CurrentPaneDomain',
+      })
+    end),
+  },
+
+  -- Toggle pane at bottom
   {
     key = '\'',
     mods = 'CTRL',
-    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+    action = wezterm.action_callback(toggle_split("Bottom")),
   },
+
+  -- Toggle pane at right
   {
     key = ']',
     mods = 'CTRL',
-    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+    action = wezterm.action_callback(toggle_split("Right")),
   },
+
+  -- Toggle pane at top
+  {
+    key = '\\',
+    mods = 'CTRL',
+    action = wezterm.action_callback(toggle_split("Top")),
+  },
+
+  -- Toggle pane at left
+  {
+    key = '[',
+    mods = 'CTRL',
+    action = wezterm.action_callback(toggle_split("Left")),
+  },
+
+  -- close current pane
   {
     key = 'w',
     mods = 'CMD',
     action = wezterm.action.CloseCurrentPane { confirm = true },
   },
-  { key = '/', mods = 'CTRL', action = act.PaneSelect },
+  { key = '/', mods = 'CTRL', action = wezterm.action.PaneSelect },
   {
     key = '=',
     mods = 'ALT',
     action = wezterm.action.ToggleFullScreen,
   },
+  -- toggle current pane full screen
   {
     key = ';',
     mods = 'CTRL',
     action = wezterm.action.TogglePaneZoomState,
-  }
+  },
+  {
+    key = "F12",
+    action = wezterm.action_callback(function(_, pane)
+        local tab = pane:tab()
+        local panes = tab:panes_with_info()
+        if #panes == 1 then
+            pane:split({
+                direction = "Bottom",
+                size = 0.5,
+            })
+        elseif not panes[1].is_zoomed then
+            panes[1].pane:activate()
+            tab:set_zoomed(true)
+        elseif panes[1].is_zoomed then
+            tab:set_zoomed(false)
+            panes[2].pane:activate()
+        end
+    end),
+  },
 }
 
 config.inactive_pane_hsb = {
