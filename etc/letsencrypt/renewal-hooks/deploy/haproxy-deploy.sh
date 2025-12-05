0dcbee1d12d0d8f1815f7a7c78844f36003cf4bf
mfirhas <fathir.irhas@gmail.com>
2025-12-05 10:41:50 +0700

    Edit

--- etc/letsencrypt/renewal-hooks/deploy/haproxy-deploy.sh
+++ etc/letsencrypt/renewal-hooks/deploy/haproxy-deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 DOMAIN="api.mfirhas.com"
 
+mkdir -p /etc/haproxy/certs
+
 # Combine certificates
 cat /etc/letsencrypt/live/$DOMAIN/fullchain.pem \
     /etc/letsencrypt/live/$DOMAIN/privkey.pem \
@@ -11,4 +13,4 @@ chmod 600 /etc/haproxy/certs/$DOMAIN.pem
 chown haproxy:haproxy /etc/haproxy/certs/$DOMAIN.pem
 
 # Reload HAProxy
-systemctl reload haproxy
+systemctl restart haproxy
