a684182a7d9b046482bca4b315cc8582ef4cfc6d
mfirhas <fathir.irhas@gmail.com>
2026-01-26 08:14:55 +0700

    Edit comment

--- infra/inits/ufw_cf_ips_public.sh
+++ infra/inits/ufw_cf_ips_public.sh
@@ -3,12 +3,12 @@
 # Make sure to deny any incoming first.
 ufw default deny incoming 
 
-# Allow cloudflare IPv4 ranges to port 443 (HTTPS)
+# Allow cloudflare IPv4 ranges to port 80 & 443
 for ip in $(curl -s https://www.cloudflare.com/ips-v4); do
     ufw allow from "$ip" to any port 80,443 proto tcp
 done
 
-# Allow cloudflare IPv6 ranges to port 443 (HTTPS)
+# Allow cloudflare IPv6 ranges to port 80 & 443
 for ip in $(curl -s https://www.cloudflare.com/ips-v6); do
     ufw allow from "$ip" to any port 80,443 proto tcp
 done
