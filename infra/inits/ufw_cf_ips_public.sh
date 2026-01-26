1dc3819386d0683195c1956da288e4ee54a6c823
mfirhas <fathir.irhas@gmail.com>
2026-01-26 08:13:22 +0700

    Add port 80 filtering

--- infra/inits/ufw_cf_ips_public.sh
+++ infra/inits/ufw_cf_ips_public.sh
@@ -5,12 +5,12 @@ ufw default deny incoming
 
 # Allow cloudflare IPv4 ranges to port 443 (HTTPS)
 for ip in $(curl -s https://www.cloudflare.com/ips-v4); do
-    ufw allow from "$ip" to any port 443 proto tcp
+    ufw allow from "$ip" to any port 80,443 proto tcp
 done
 
 # Allow cloudflare IPv6 ranges to port 443 (HTTPS)
 for ip in $(curl -s https://www.cloudflare.com/ips-v6); do
-    ufw allow from "$ip" to any port 443 proto tcp
+    ufw allow from "$ip" to any port 80,443 proto tcp
 done
 
 echo "Cloudflare IPs added to ufw for port 443 (HTTPS)"
